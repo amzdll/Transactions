0d362a815962bc01fed7e3d72b1d001860f683af
glenpoin <glenpoin@student.21-school.ru>
2023-10-24 04:19:50 +0300

    first steps

--- src/include/abstract_storage.h
+++ src/include/abstract_storage.h
@@ -8,18 +8,18 @@ namespace s21 {
 template <class Key, class Value>
 class AbstractStorage {
  public:
-  virtual bool Set(Value value) = 0;
+  virtual bool Set(Key key, Value value) = 0;
   virtual Value Get(Key key) = 0;
   virtual bool Exists(Key key) = 0;
-  virtual bool Del(Key key) = 0;
-  virtual void Update() = 0;
-  virtual std::vector<std::string> Keys() = 0;
-  virtual void Rename(Key old_name, Key new_name) = 0;
-  virtual unsigned int TTL(Key key) = 0;
-  virtual std::vector<Key> Find(Value) = 0;
-  virtual std::vector<Value> ShowAll() = 0;
-  virtual int Upload(std::string file_path) = 0;
-  virtual int Export(std::string file_path) = 0;
+//  virtual bool Del(Key key) = 0;
+//  virtual void Update() = 0;
+//  virtual std::vector<std::string> Keys() = 0;
+//  virtual void Rename(Key old_name, Key new_name) = 0;
+//  virtual unsigned int TTL(Key key) = 0;
+//  virtual std::vector<Key> Find(Value) = 0;
+//  virtual std::vector<Value> ShowAll() = 0;
+//  virtual int Upload(std::string file_path) = 0;
+//  virtual int Export(std::string file_path) = 0;
 
  protected:
  private:
--- src/include/hash_table.h
+++ src/include/hash_table.h
@@ -1,39 +1,61 @@
 #ifndef TRANSACTIONS_SRC_INCLUDE_HASH_TABLE_H_
 #define TRANSACTIONS_SRC_INCLUDE_HASH_TABLE_H_
+#include "iostream"
 
 #include "abstract_storage.h"
+#include <type_traits>
 
 #include <vector>
 #include <list>
 
 namespace s21 {
-template <class Key, class Value>
+template<typename Key, typename Value>
 class HashTable : AbstractStorage<Key, Value> {
  public:
-  using bucket = std::list<Value>;
+  using bucket = std::list<std::pair<Key, Value>>;
 
   HashTable();
-  ~HashTable();
-
-  bool Set(Value value) override;
+//  ~HashTable();
+//
+  void print();
+  bool Set(Key key, Value value) override;
   Value Get(Key key) override;
   bool Exists(Key key) override;
-  bool Del(Key key) override;
-  void Update() override;
-  std::vector<std::string> Keys() override;
-  void Rename(Key old_name, Key new_name) override;
-  unsigned int TTL(Key key) override;
-  std::vector<Key> Find(Value) override;
-  std::vector<Value> ShowAll() override;
-  int Upload(std::string file_path) override;
-  int Export(std::string file_path) override;
+//  bool Del(Key key) override;
+//  void Update() override;
+//  std::vector<std::string> Keys() override;
+//  void Rename(Key old_name, Key new_name) override;
+//  unsigned int TTL(Key key) override;
+//  std::vector<Key> Find(Value) override;
+//  std::vector<Value> ShowAll() override;
+//  int Upload(std::string file_path) override;
+//  int Export(std::string file_path) override;
+
+//  unsigned int Hash(Key key);
+//  unsigned int Hash(std::string key);
+
+  size_t Hash(Key key);
 
  private:
+  size_t HashNumeric(Key key);
+  size_t HashString(std::string key);
+
   std::vector<bucket> buckets_{};
 
-  int HashFunction();
-
 };
+
+
+template<typename Key, typename Value>
+void HashTable<Key, Value>::print() {
+  for (int i = 0; i < buckets_.size(); ++i) {
+    std::cout << "bucket - " << i << " {";
+    for (auto itr : buckets_[i]) {
+      std::cout << " " << itr.first << ":" << itr.second << " ";
+    }
+    std::cout << "}" << std::endl;
+  }
+}
+
 }  // namespace s21
 
 #endif  // TRANSACTIONS_SRC_INCLUDE_HASH_TABLE_H_
