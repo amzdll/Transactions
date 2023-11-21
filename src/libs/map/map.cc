c5ff535e0e762a9ae93d38bee2c276da8931ad15
glenpoin <glenpoin@student.21-school.ru>
2023-11-22 02:20:44 +0300

    add map

---
+++ src/libs/map/map.cc
@@ -0,0 +1,7 @@
+//
+// Created by freiqq on 22.11.23.
+//
+
+#include "map.h"
+
+namespace s21 {}  // namespace s21
---
+++ src/libs/map/map.h
@@ -0,0 +1,14 @@
+//
+// Created by freiqq on 22.11.23.
+//
+
+#ifndef TRANSACTIONS_SRC_LIBS_MAP_MAP_H_
+#define TRANSACTIONS_SRC_LIBS_MAP_MAP_H_
+
+namespace s21 {
+
+class map {};
+
+}  // namespace s21
+
+#endif  // TRANSACTIONS_SRC_LIBS_MAP_MAP_H_
